787bcd8f37c5fd6c9dd3737a0d3fee089368ba49
unknown <estjunbeom@gmail.com>
2021-01-09 23:00:54 +0900

    FinanceDataReader

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ import FinanceDataReader as fdr
 import warnings
 import os
 
-URL = "C:\\perdictShareprice\\share csv\\01-삼성전자-주가.csv"
+# URL = "C:\\perdictShareprice\\share csv\\01-삼성전자-주가.csv"
 # val.pltlib(URL)
 # %matplotlib inline
 warnings.filterwarnings('ignore')
 plt.rcParams['font.family'] = 'NanumGothic'
 
 # 삼성전자(005930) 전체 (1996-11-05//2017 ~ 현재)
-samsung = fdr.DataReader('005930', '2017')
+samsung = fdr.DataReader('005930')
 # Open: 시가
 # High: 고가
 # Low: 저가
@@ -29,11 +29,28 @@ samsung = fdr.DataReader('005930', '2017')
 samsung['Year'] = samsung.index.year
 samsung['Month'] = samsung.index.month
 samsung['Day'] = samsung.index.day
-print(samsung.head())
+
 plt.figure(figsize=(16, 9))
 sns.lineplot(y=samsung['Close'], x=samsung.index)
 plt.xlabel('time')
 plt.ylabel('price')
+time_steps = [['1990', '2000'],
+              ['2000', '2010'],
+              ['2010', '2015'],
+              ['2015', '2020']]
+
+fig, axes = plt.subplots(2, 2)
+fig.set_size_inches(16, 9)
+
+for i in range(4):
+    ax = axes[i//2, i % 2]
+    df = samsung.loc[(samsung.index > time_steps[i][0]) &
+                     (samsung.index < time_steps[i][1])]
+    sns.lineplot(y=df['Close'], x=df.index, ax=ax)
+    ax.set_title(f'{time_steps[i][0]}~{time_steps[i][1]}')
+    ax.set_xlabel('time')
+    ax.set_ylabel('price')
+plt.tight_layout()
 plt.show()
 # Shareprice = pd.read_csv(
 #     "C:\\perdict Share price\\share csv\\01-삼성전자-주가.csv")
