abcb475f36349e23a3676372bba01fec778a525f
unknown <estjunbeom@gmail.com>
2021-01-11 01:40:04 +0900

    Data visualization

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import tensorflow as tf
 import pandas as pd
-import val
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 import FinanceDataReader as fdr
 import warnings
 import os
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.layers import LSTM
+from tensorflow.keras.layers import Dense, LSTM, Conv1D, Lambda
+from tensorflow.keras.losses import Huber
+from tensorflow.keras.optimizers import Adam
 
-# URL = "C:\\perdictShareprice\\share csv\\01-삼성전자-주가.csv"
-# val.pltlib(URL)
+URL = "C:\\perdictShareprice\\share csv\\01-삼성전자-주가.csv"
+
 # %matplotlib inline
 warnings.filterwarnings('ignore')
 plt.rcParams['font.family'] = 'NanumGothic'
@@ -26,6 +34,7 @@ samsung = fdr.DataReader('005930')
 # print(samsung.tail())
 # print(samsung.head())
 # print(samsung.index)
+
 samsung['Year'] = samsung.index.year
 samsung['Month'] = samsung.index.month
 samsung['Day'] = samsung.index.day
@@ -51,7 +60,120 @@ for i in range(4):
     ax.set_xlabel('time')
     ax.set_ylabel('price')
 plt.tight_layout()
+# plt.show()
+
+# 정규화작업
+scaler = MinMaxScaler()
+scale_cols = ['Open', 'High', 'Low', 'Close', 'Volume']
+scaled = scaler.fit_transform(samsung[scale_cols])
+# print(scaled)
+
+df = pd.DataFrame(scaled, columns=scale_cols)
+
+
+# train /test 분할
+x_train, x_test, y_train, y_test = train_test_split(
+    df.drop('Close', 1), df['Close'], test_size=0.2, random_state=0, shuffle=False)
+# print(x_train.shape, y_train.shape)
+# print(x_test.shape, y_test.shape)
+# print(x_train)
+
+# 데이터 시퀀스 데이터셋 구성
+
+
+def dataset(series, window_size, batch_size, shuffle):
+    series = tf.expand_dims(series, axis=-1)
+    ds = tf.data.Dataset.from_tensors(series)
+    ds = ds.window(window_size + 1, shift=1, drop_remainder=True)
+    ds = ds.flat_map(lambda w: w.batch(window_size + 1))
+    if shuffle:
+        ds = ds.shuffle(1000)
+    ds = ds.map(lambda w: (w[:-1], w[-1]))
+    return ds.batch(batch_size).prefetch(1)
+
+
+WINDOW_SIZE = 20
+BATCH_SIZE = 32
+
+# trian_data는 학습용 데이터셋, test_data는 검증용 데이터셋
+train_data = dataset(y_train, WINDOW_SIZE, BATCH_SIZE, True)
+test_data = dataset(y_test, WINDOW_SIZE, BATCH_SIZE, False)
+# for data in train_data.take(1):
+#     print(f'데이터셋(X) 구성(batch_size, window_size, feature갯수): {data[0].shape}')
+#     print(f'데이터셋(Y) 구성(batch_size, window_size, feature갯수): {data[1].shape}')
+
+model = Sequential()
+model.add(LSTM(16, activation='tanh'),
+          )
+model.add(Conv1D(filters=32, kernel_size=5,
+                 padding="causal",
+                 activation="relu",
+                 input_shape=[WINDOW_SIZE, 1]
+                 ),)
+model.add(Dense(1))
+# model = tf.keras.models.Sequential([
+#     tf.keras.layers.Conv1D(filters=32, kernel_size=5,
+#                            padding="causal",
+#                            activation="relu",
+#                            input_shape=[WINDOW_SIZE, 1]
+#                            ),
+#     # LSTM
+#     tf.keras.layers.LSTM(16, activation='tanh'),
+#     tf.keras.layers.Dense(16, activation="relu"),
+#     tf.keras.layers.Dense(1),
+# ])
+
+
+# 스퀀스 학습 = Huber
+# loss=tf.keras.losses.Huber()
+optimizer = Adam(0.0005)
+model.compile(loss=tf.keras.losses.Huber(),
+              optimizer=optimizer, metrics=['mse'])
+
+# earlystopping= 10번 epoching  val_loss 개선이 없다면 학습을 멈춤
+# earlystopping = EarlyStopping(
+#     monitor='val_loss', patience=10)
+# # val_loss 기준 체크 포인터
+# # filename = os.path.join('tmp', 'tmp_checkpoint.h5')
+# filename = os.path.join('tmp', 'checkpointer.ckpt')
+# checkpoint = ModelCheckpoint(
+#     filename, monitor='val_loss', verbose=1, save_best_only=True, mode='auto')
+# # checkpoint = ModelCheckpoint(filename,
+# #                              save_weights_only=True,
+# #                              save_best_only=True,
+# #                              monitor='val_loss',
+# #                              verbose=1,
+# #                              mode='auto')
+
+# early_stop = EarlyStopping(monitor='val_loss', patience=5)
+# # history = model.fit(x_train, y_train,
+# #                     epochs=50,
+# #                     batch_size=16,
+# #                     validation_data=(x_valid, y_valid),
+# #                     callbacks=[early_stop, checkpoint],
+# #                     verbose=1)
+
+# history = model.fit(train_data,
+#                     epochs=50,
+#                     batch_size=16,
+#                     validation_data=(test_data),
+#                     callbacks=[early_stop, checkpoint],
+#                     )
+
+# model.load_weights(filename)
+
+pred = model.predict(test_data)
+print(pred.shape)
+
+
+# 예측 데이터 시각화 20일치를 이용하여 21일을 예측
+plt.figure(figsize=(12, 9))
+plt.plot(np.asarray(y_test)[20:], label='actual')
+plt.plot(pred, label='prediction')
+plt.legend()
 plt.show()
+
+
 # Shareprice = pd.read_csv(
 #     "C:\\perdict Share price\\share csv\\01-삼성전자-주가.csv")
 # Shareprice.head()
--- val.py
+++ val.py
@@ -3,19 +3,117 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+import FinanceDataReader as fdr
 import warnings
 import os
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.layers import LSTM
+
+data_path = "C:\\perdictShareprice\\share csv\\"
+df_price = pd.read_csv(os.path.join(
+    data_path, '01-삼성전자-주가.csv'), encoding='utf8')
+print(df_price.describe())
+
+pd.to_datetime(df_price['일자'], format='%Y%m%d')
+# 0      2020-01-07
+# 1      2020-01-06
+# 2      2020-01-03
+# 3      2020-01-02
+# 4      2019-12-30
+
+df_price['일자'] = pd.to_datetime(df_price['일자'], format='%Y%m%d')
+df_price['연도'] = df_price['일자'].dt.year
+df_price['월'] = df_price['일자'].dt.month
+df_price['일'] = df_price['일자'].dt.day
 
 
-def pltlib(URL):
-    se = pd.read_csv(URL)
-    # pt.figure(figsize=(12, 8))
-    # pt.plot(x=se['일자'], y=se['종가'])
-    # pt.xticks(rotation=75)
+plt.figure(figsize=(16, 9))
+sns.lineplot(y=df_price['종가'], x=df_price['일자'])
+plt.xlabel('time')
+plt.ylabel('price')
+# plt.show()
 
-    plt.figure(figsize=(16, 9))
-    se['일자'] = pd.to_datetime(se['일자'], format='%Y%m%d')
-    sns.lineplot(y=se['종가'], x=se['일자'])
-    plt.xlabel('time')
-    plt.ylabel('price')
-    plt.show()
+scaler = MinMaxScaler()
+scale_cols = ['시가', '고가', '저가', '종가', '거래량']
+df_scaled = scaler.fit_transform(df_price[scale_cols])
+
+df_scaled = pd.DataFrame(df_scaled)
+df_scaled.columns = scale_cols
+
+print(df_scaled)
+TEST_SIZE = 200  # 200일 이전의 데이터로 학습
+train = df_scaled[:-TEST_SIZE]
+test = df_scaled[-TEST_SIZE:]
+
+
+def make_dataset(data, label, window_size=20):
+    feature_list = []
+    label_list = []
+    for i in range(len(data) - window_size):
+        feature_list.append(np.array(data.iloc[i:i+window_size]))
+        label_list.append(np.array(label.iloc[i+window_size]))
+    return np.array(feature_list), np.array(label_list)
+
+
+feature_cols = ['시가', '고가', '저가', '거래량']
+label_cols = ['종가']
+
+train_feature = train[feature_cols]
+train_label = train[label_cols]
+test_feature = train[feature_cols]
+test_label = train[label_cols]
+# train dataset
+train_feature, train_label = make_dataset(train_feature, train_label, 20)
+
+# train, validation set 생성
+x_train, x_valid, y_train, y_valid = train_test_split(
+    train_feature, train_label, test_size=0.2)
+
+x_train.shape, x_valid.shape
+# ((6086, 20, 4), (1522, 20, 4))
+
+# test dataset (실제 예측 해볼 데이터)
+test_feature, test_label = make_dataset(test_feature, test_label, 20)
+test_feature.shape, test_label.shape
+# ((180, 20, 4), (180, 1))
+
+model = Sequential()
+model.add(LSTM(16,
+               input_shape=(train_feature.shape[1], train_feature.shape[2]),
+               activation='relu',
+               return_sequences=False)
+          )
+model.add(Dense(1))
+optimizer = tf.keras.optimizers.Adam(0.0005)
+model.compile(loss=tf.keras.losses.Huber(),
+              optimizer=optimizer, metrics=['mse'])
+
+early_stop = EarlyStopping(monitor='val_loss', patience=5)
+filename = os.path.join('tmp', 'tmp_checkpoint.h5')
+checkpoint = ModelCheckpoint(
+    filename, monitor='val_loss', verbose=1, save_best_only=True, mode='auto')
+
+history = model.fit(x_train, y_train,
+                    epochs=50,
+                    batch_size=16,
+                    validation_data=(x_valid, y_valid),
+                    callbacks=[early_stop, checkpoint],
+                    verbose=1)
+
+# weight 로딩
+model.load_weights(filename)
+
+# 예측
+pred = model.predict(test_feature)
+print(pred)
+plt.figure(figsize=(12, 9))
+plt.plot(test_label, label='actual')
+plt.plot(pred, label='prediction')
+plt.xlabel('time')
+plt.ylabel('price')
+plt.legend()
+plt.show()
